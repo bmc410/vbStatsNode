bf9745657f677de40cd9c82289a35b85e03968bc
Bill McCoy <bmc410@comcast.net>
2021-01-04 17:45:18 -0500

    j

--- controllers/TodosController.ts
+++ controllers/TodosController.ts
@@ -4,15 +4,32 @@ class TodosController {
 
     public async get(req, res) {
         try {
-            const client = await pool.connect();
+            //const client = await pool.connect();
 
             const sql = "SELECT * FROM players";
-            const { rows } = await client.query(sql);
-            const todos = rows;
+            //const { rows } = await client.query(sql);
+            //const todos = rows;
 
-            client.release();
+            //client.release();
+            const products = [
+                {
+                  id: 1,
+                  name: "hammer",
+                },
+                {
+                  id: 2,
+                  name: "screwdriver",
+                },
+                ,
+                {
+                  id: 3,
+                  name: "wrench",
+                },
+              ];
+            
+             var j = JSON.stringify(products);
 
-            res.send(todos);
+            res.send(products);
         } catch (error) {
             res.status(400).send(error);
         }
