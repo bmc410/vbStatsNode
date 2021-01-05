47300256ea84cc6c04c3649b07ca58ffb48cb89b
Bill McCoy <bmc410@comcast.net>
2021-01-04 19:38:28 -0500

    d

--- dbconfig/dbconnector.ts
+++ dbconfig/dbconnector.ts
@@ -6,8 +6,11 @@ const pool = new Pool ({
     //connectionString: 'postgres://mtbijyawwysjzy:a629bbf09042ec1f8014a630864202e5fe9aa4bcb7afa59901035f50a988a88c@ec2-3-231-241-17.compute-1.amazonaws.com:5432/d46kgtuu4i1a7',
     //connectionString: 'postgres://bmc410:Cocacola1!]@localhost:5432/vbStats',
     idleTimeoutMillis: 30000,
-    ssl: true,
-    rejectUnauthorized: false
+    ssl: {
+        rejectUnauthorized: false
+      }
+    //ssl: true,
+    //rejectUnauthorized: false
 });
 
 export default pool;
